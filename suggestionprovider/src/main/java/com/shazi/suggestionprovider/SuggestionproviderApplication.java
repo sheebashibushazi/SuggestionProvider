94b5925fe733f691366002b5b744bef7d1939278
USER <USER@192.168.0.4>
2020-09-25 00:19:08 +0530

    first required files

---
+++ suggestionprovider/src/main/java/com/shazi/suggestionprovider/SuggestionproviderApplication.java
@@ -0,0 +1,13 @@
+package com.shazi.suggestionprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuggestionproviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuggestionproviderApplication.class, args);
+	}
+
+}
---
+++ suggestionprovider/src/test/java/com/shazi/suggestionprovider/SuggestionproviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shazi.suggestionprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuggestionproviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
